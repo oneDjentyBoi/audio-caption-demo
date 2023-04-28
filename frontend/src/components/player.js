0c7d2224620c860c4a2a28d5694e7a239d353107
Aritro Banerjee <aritrorik@gmail.com>
2023-04-28 23:07:04 +0530

    add transcript highlight

--- frontend/src/components/player.js
+++ frontend/src/components/player.js
@@ -4,7 +4,7 @@ import audioSrc from "./audio1.mp3";
 
 const sampleText = {//arranged in increasing order wrt end time
     3:{
-        text: "hello",
+        text: "Hello,",
         colour:"green"
     },
     10:{
@@ -12,11 +12,11 @@ const sampleText = {//arranged in increasing order wrt end time
         colour:"blue"
     },
     17:{
-        text: "is Aritro",
+        text: "is Aritro.",
         colour:"purple"
     },
     28:{
-        text: "Nice to meet you",
+        text: "Nice to meet you.",
         colour:"red"
     },
     40:{
@@ -25,8 +25,9 @@ const sampleText = {//arranged in increasing order wrt end time
     }
 }
 
+const sampleTextArr = Object.keys(sampleText);
+
 const timeStamps = (time) => {// calculates start-time and end-time of a phrase
-    let sampleTextArr = Object.keys(sampleText);
     let startTime = 0, endTime = 0;
     for(let i = 0; i < sampleTextArr.length; i++){
         if(parseFloat(sampleTextArr[i]) > time){
@@ -46,27 +47,47 @@ const Player = () => {
     const playerRef = useRef();
     const captionRef = useRef();
     useEffect(() => {
+        const clearBg = (idx) => {
+            captionRef.current.childNodes[idx].style.backgroundColor = 'transparent';
+        }
         const onTimeUpdate = () => {
             let time = playerRef.current.currentTime;
             let {startTime, endTime} = timeStamps(time);
-
+            var spanIdx = 0;
             if(time > startTime && time < endTime){
-                captionRef.current.innerText = sampleText[endTime].text;
-                captionRef.current.style.color = sampleText[endTime].colour;
+                spanIdx = sampleTextArr.findIndex( txt => String(endTime) === txt );
+                if(spanIdx && spanIdx !== -1)
+                    clearBg(spanIdx - 1);
+                if(spanIdx !== -1){
+                    captionRef.current.childNodes[spanIdx].style.backgroundColor = sampleText[endTime].colour;
+                    if(spanIdx + 1 < captionRef.current.childNodes.length){
+                        clearBg(spanIdx + 1);
+                    }
+                }
             }
-            else
-                captionRef.current.innerText = ''; //cleanup
+            if(time > String(sampleTextArr[sampleTextArr.length - 1]))
+                clearBg(sampleTextArr.length - 1);
         };
+        const seekerFunc = () => {
+            captionRef.current.childNodes.forEach(childNode => {
+                childNode.style.backgroundColor = 'transparent';           
+            });
+        }
         playerRef.current.addEventListener("timeupdate", onTimeUpdate);
+        playerRef.current.addEventListener("seeking", seekerFunc);
         return () => {
             playerRef.current
             .removeEventListener("timeupdate", onTimeUpdate);
+            playerRef.current
+            .removeEventListener("seeking",seekerFunc);
         }
     }, []);
     
     return (<React.Fragment>
             <audio controls src={audioSrc} ref={playerRef} type="audio/mp3"/>
-            <div ref={captionRef} className='caption-container'></div>
+            <div ref={captionRef} className='caption-container'>
+                { sampleTextArr.map((wordKey, i) => <span key={`${wordKey}-${i}`}>{sampleText[wordKey].text.trim()} </span>) }
+            </div>
     </React.Fragment>
     )
 }
